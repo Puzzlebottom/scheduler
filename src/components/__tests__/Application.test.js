f8eca40dd3224ff64b1c74a4c281b6609ae7aa6e
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-17 09:14:48 -0400

    improves test coverage

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { render, getByText, getAllByTestId, getByAltText, fireEvent, waitForElement, waitForElementToBeRemoved, getByPlaceholderText, queryByText } from "@testing-library/react";
 import Application from "components/Application";
 
 describe('Application', () => {
@@ -13,4 +13,45 @@ describe('Application', () => {
     expect(getByText(/leopold silvers/i)).toBeInTheDocument();
   });
 
+  it('loads data, books an interview and reduces the spots remaining for the first day by 1', async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, /archie cohen/i));
+
+    const appointment = getAllByTestId(container, 'appointment')[0];
+
+
+    fireEvent.click(getByAltText(appointment, /add/i));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), { target: { value: 'Lydia Miller-Jones' } });
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+    fireEvent.click(getByText(appointment, /save/i));
+
+    // console.log(debug());
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => getByText(appointment, /saving/i));
+
+    expect(getByText(appointment, 'Lydia Miller-Jones')).toBeInTheDocument();
+
+    const mondayDayListItem = getAllByTestId(container, 'day').find(day => queryByText(day, /monday/i));
+
+    expect(getByText(mondayDayListItem, /no spots remaining/i)).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Delete" button on the first booked appointment.
+    // 4. Check that the confirmation message is shown.
+    // 5. Click the "Confirm button".
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the text "Deleting disappears".
+    // 8. Check that the DayListItem with the text "Monday" has increased by 1".
+  });
 });
+
+
